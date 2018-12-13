6989a29a84099b3df26bdaa503977d07723ec58a
Charles Lane <charleslane23@gmail.com>
2018-12-13 08:42:08 -0500

    header working

--- Backend/frontend/src/components/Header.js
+++ Backend/frontend/src/components/Header.js
@@ -4,17 +4,6 @@ import config from "../config.js";
 import { mapIndexed } from "../helpers.js";
 import $ from 'jquery'
 
-let toggle = true
-$('.nav-wrapper').click(function(){
-    console.log('hello')
-    if(toggle){
-        $('#mobile-demo').css({'transform': 'translateX(0%)'})
-
-    }else {
-        $('#mobile-demo').css({'transform': 'translateX(-105%)'})
-    }
-    toggle = !toggle
-})
 const Header = ( ) =>
   config.header ? (
     <p>Nothing to show</p>
@@ -42,31 +31,19 @@ const Header = ( ) =>
                 </li>
             </ul>
         </div>
-            <nav className="nav-extended">
-                <div className="nav-wrapper">
-                <a href="/" className="brand-logo"><img src={config.brandImg}/></a>
-                {/* <a  id="menu-icon-header"className="sidenav-trigger">
-                    <i className="material-icons">menu</i></a> */}
-                <span id="menu-icon-headers">
-                    <i className="material-icons">menu</i>
-                </span>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {
+
+         <ul class="topnav">
+            <li><a href="/" className="brand-logo"><img src={config.brandImg}/></a></li>
+            {
                         mapIndexed((x, i)=> (
-                            <li key={i}><a  href={x['path']}>{x['name']}</a></li>
+                            <li className="right"key={i}>
+                                <a href={x['path']}>{x['name']}</a>
+                            </li>
                         ))(config.pages)
-                    }
-                </ul>
-                </div>
-            </nav>
-            <ul className="sidenav show-on-med-and-down" id="mobile-demo">
-                {
-                        mapIndexed((x, i)=> (
-                            <li key={i}><a href={x['path']}>{x['name']}</a></li>
-                        ))(config.pages)
-                    }
-         </ul>
-      </div>
+            }
+        </ul>
+  
+      </div>  
 
   );
 
