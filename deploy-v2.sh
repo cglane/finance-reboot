d1326db6014d049439949e17d6c3b117cd46676c
Charles Lane <charleslane23@gmail.com>
2019-02-08 07:22:13 -0500

    hello

--- deploy-v2.sh
+++ deploy-v2.sh
@@ -15,7 +15,7 @@ npm run build
 
 aws s3 cp Backend/frontend/static/frontend/main.js s3://hfl-static-backend/static/frontend/main.js --acl public-read
 
-
+#
 #  cd ./Backend
-
+#
 #  eb deploy hfl-server-side-two
